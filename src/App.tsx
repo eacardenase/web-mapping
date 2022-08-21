cc60d7132d9e4d782d57e4e9964ec5fa3fd591cb
Edwin Alexander Cardenas Esteban <eacardenase@unal.edu.co>
2022-08-21 08:02:10 -0500

    basic setup displaying geojson data

--- src/App.tsx
+++ src/App.tsx
@@ -13,16 +13,53 @@ const App = () => {
     const [features, setFeatures] = useState<Feature<Geometry>[]>([]);
 
     const fetchData = async () => {
-        const response = await fetch('/mock-geojson-api.json');
+        const barriosData = await fetch('/mapsData/Barrios.geojson');
+        const prediosData = await fetch('/mapsData/Predios.geojson');
+        const barrios4326Data = await fetch('/mapsData/Barrios-4326.geojson');
+        const construcctionesRuralData = await fetch(
+            '/mapsData/ConstruccionesRural.geojson'
+        );
+        const construcctionesUrbanoData = await fetch(
+            '/mapsData/ConstruccionesUrbano.geojson'
+        );
+        const prediosRuralData = await fetch('/mapsData/PrediosRural.geojson');
+        const prediosUrbanoData = await fetch(
+            '/mapsData/PrediosUrbano.geojson'
+        );
+        const veredasData = await fetch('/mapsData/Veredas.geojson');
+        const zonaEconomicaRuralData = await fetch(
+            '/mapsData/ZonaEconomicaRural.geojson'
+        );
+        const zonaEconomicaUrbanaData = await fetch(
+            '/mapsData/ZonaEconomicasUrb.geojson'
+        );
+        const zonaFisicaRuralData = await fetch(
+            '/mapsData/ZonaFisicaRural.geojson'
+        );
+        const zonaFisicaUrbanoData = await fetch(
+            '/mapsData/ZonaFisicaUrbano.geojson'
+        );
 
-        const fetchedFeatures = await response.json();
+        const barrios = await barriosData.json();
+        const barrios4326 = await barrios4326Data.json();
+        const predios = await prediosData.json();
+        const construccionesRural = await construcctionesRuralData.json();
+        const construccionesUrbano = await construcctionesUrbanoData.json();
+        const prediosRural = await prediosRuralData.json();
+        const prediosUrbano = await prediosUrbanoData.json();
+        const veredas = await veredasData.json();
+        const zonaEconomicaRural = await zonaEconomicaRuralData.json();
+        const zonaEconomicaUrbana = await zonaEconomicaUrbanaData.json();
+        const zonaFisicaRural = await zonaFisicaRuralData.json();
+        const zonaFisicaUrbana = await zonaFisicaUrbanoData.json();
 
         const wktOptions = {
             dataProjection: 'EPSG:4326',
             featureProjection: 'EPSG:3857',
         };
+
         const parsedFeatures = new GeoJSON().readFeatures(
-            fetchedFeatures,
+            barrios4326,
             wktOptions
         );
 
--- src/components/MapWrapper.tsx
+++ src/components/MapWrapper.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef, ChangeEvent } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 // openlayers
 import Map from 'ol/Map';
@@ -8,131 +8,79 @@ import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
 import XYZ from 'ol/source/XYZ';
 import { transform } from 'ol/proj';
-import { Coordinate, toStringXY } from 'ol/coordinate';
+import { Coordinate } from 'ol/coordinate';
 import OSM from 'ol/source/OSM';
 import Geometry from 'ol/geom/Geometry';
 
 import './MapWrapper.css';
+import { fromLonLat } from 'ol/proj';
 
-const USGSTopo = new TileLayer({
-    source: new XYZ({
-        url: 'https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}',
-    }),
-});
-
-const googleMapsTerrain = new TileLayer({
-    source: new XYZ({
-        url: 'http://mt0.google.com/vt/lyrs=p&hl=en&x={x}&y={y}&z={z}',
-    }),
-});
-
-const osmLayer = new TileLayer({
-    source: new OSM(),
-});
-
-function MapWrapper({ features }) {
-    // set intial state
-    const [map, setMap] = useState();
+function MapWrapper({ features }: any) {
+    const [map, setMap] = useState<Map>();
     const [featuresLayer, setFeaturesLayer] =
         useState<VectorLayer<VectorSource<Geometry>>>();
     const [selectedCoord, setSelectedCoord] = useState<Coordinate>();
 
-    // pull refs
     const mapElement = useRef();
 
-    // create state ref that can be accessed in OpenLayers onclick callback function
-    //  https://stackoverflow.com/a/60643670
     const mapRef = useRef();
     mapRef.current = map;
 
-    // initialize map on first render - logic formerly put into componentDidMount
     useEffect(() => {
-        // create and add vector source layer
         const initalFeaturesLayer = new VectorLayer({
-            source: new VectorSource(),
+            source: new VectorSource(features),
         });
+
+        const itaguiLonLat = [-75.59913, 6.18461];
+        const itaguiWebMercator = fromLonLat(itaguiLonLat);
 
         // create map
         const initialMap = new Map({
             target: mapElement.current,
+            view: new View({
+                projection: 'EPSG:3857',
+                center: itaguiWebMercator,
+                zoom: 13,
+            }),
             layers: [
-                // USGS Topo
+                // Google Maps Terrain
                 new TileLayer({
                     source: new XYZ({
-                        url: 'https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}',
+                        url: 'http://mt0.google.com/vt/lyrs=p&hl=en&x={x}&y={y}&z={z}',
                     }),
                 }),
 
-                // Google Maps Terrain
-                /* new TileLayer({
-          source: new XYZ({
-            url: 'http://mt0.google.com/vt/lyrs=p&hl=en&x={x}&y={y}&z={z}',
-          })
-        }), */
+                // new TileLayer({
+                //     source: new OSM(),
+                // }),
 
                 initalFeaturesLayer,
             ],
-            view: new View({
-                projection: 'EPSG:3857',
-                center: [0, 0],
-                zoom: 3,
-            }),
             controls: [],
         });
 
-        // set map onclick handler
-        initialMap.on('click', handleMapClick);
-
-        // save map and vector layer references to state
         setMap(initialMap);
         setFeaturesLayer(initalFeaturesLayer);
     }, []);
 
-    // update map if features prop changes - logic formerly put into componentDidUpdate
     useEffect(() => {
         if (features.length) {
-            // may be null on first render
-
-            // set features to map
             featuresLayer &&
                 featuresLayer.setSource(
                     new VectorSource({
-                        features: features, // make sure features is an array
+                        features: features,
                     })
                 );
 
-            // fit map to feature extent (with 100px of padding)
-            map?.getView().fit(featuresLayer.getSource().getExtent(), {
+            map?.getView().fit(featuresLayer!.getSource()!.getExtent(), {
                 padding: [100, 100, 100, 100],
             });
         }
     }, [features]);
 
-    // map click handler
-    const handleMapClick = (event) => {
-        // get clicked coordinate using mapRef to access current React state inside OpenLayers callback
-        //  https://stackoverflow.com/a/60643670
-        const clickedCoord = mapRef.current.getCoordinateFromPixel(event.pixel);
-
-        // transform coord to EPSG 4326 standard Lat Long
-        const transormedCoord = transform(
-            clickedCoord,
-            'EPSG:3857',
-            'EPSG:4326'
-        );
-
-        // set React state
-        setSelectedCoord(transormedCoord);
-    };
-
-    // render component
     return (
         <div>
             <div ref={mapElement} className="map-container"></div>
-
-            <div className="clicked-coord-label">
-                <p>{selectedCoord ? toStringXY(selectedCoord, 5) : ''}</p>
-            </div>
         </div>
     );
 }
